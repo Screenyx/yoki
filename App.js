e3ce0efb672abb01f7a155e5a6d028f53c022552
Screenyx <mercier.hugo14@gmail.com>
2022-04-07 11:20:17 +0200

    ex 2

--- App.js
+++ App.js
@@ -1,37 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, ScrollView, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <ScrollView style={styles.container}>
       <View style={styles.container}>
-        <Text style={styles.title}>Exercice 1</Text>
+        <Image
+          style={styles.image}
+          source={require('./assets/500.png')}
+        />
       </View>
-      <View style={styles.container}>
-        <View style={styles.container}>
-          <Text style={styles.miniTitle}>Mon titre</Text>
-        </View>
-        <View style={styles.container}>
-          <Text style={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat tortor in turpis sagittis molestie. Phasellus sagittis leo nisl, in maximus purus elementum vel. Vestibulum sodales metus dictum erat fringilla egestas.
-          </Text>
-        </View>
-        <View style={styles.container}>
-          <Text style={styles.miniTitle}>Mon titre</Text>
-        </View>
-        <View style={styles.container}>
-          <Text style={styles.text}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat tortor in turpis sagittis molestie. Phasellus sagittis leo nisl, in maximus purus elementum vel. Vestibulum sodales metus dictum erat fringilla egestas.
-          </Text>
-        </View>
-        <View style={styles.container}>
-          <Text style={styles.miniTitle}>Mon titre</Text>
-        </View>
-        <View style={styles.container}>
-          <Text style={styles.text}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat tortor in turpis sagittis molestie. Phasellus sagittis leo nisl, in maximus purus elementum vel. Vestibulum sodales metus dictum erat fringilla egestas.
-          </Text>
-        </View>
+      <View style={styles.container} >
+        <Text style={styles.title}>Lorem ipsum dolor sit amet, consectetur adipiscing elit</Text>
+      </View>
+      <View>
+      <Text style={styles.description}>By Hugo</Text>
+      </View>
+      <View>
+        <Pressable style={styles.button}>
+          <Text>Like</Text>
+        </Pressable>
+      </View>
+      <View>
+      <Text style={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+      </View>
+      <View>
+      <Text style={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+      </View>
+      <View>
+      <Text style={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
       </View>
 
       <StatusBar style="auto" />
@@ -45,30 +42,29 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
    
   },
-  text: {
-    color: 'black',
-    marginHorizontal: 40,
-    marginTop: 20,
-
+  image: {
+    height: 300,
+    width: 450,
   },
   title: {
-    textAlign: 'center',
-    fontSize: 30,
     color: 'black',
+    fontSize: 20,
+    marginTop: 20,
+    marginHorizontal: 30,
+  },
+  description: {
+    color: 'grey',
+    marginTop: 20,
+    fontSize: 15,
+    marginHorizontal: 30,
+  },
+  button: {
+    marginLeft: 40,
+    marginRight: 310,
     borderColor: 'black',
     borderWidth: 1,
-    paddingTop: 10,
-    marginTop: 10,
-
-  },
-  miniTitle: {
-    marginHorizontal: 20,
-    marginTop: 30,
-    fontSize: 20,
-    color: 'black',
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
-    paddingBottom: 20,
-    paddingLeft: 20,
+    borderRadius: 4,
+    alignItems: 'center',
   }
+
 });
