58b74bb14f713e37a3ac522b44e26550dbf49447
Screenyx <mercier.hugo14@gmail.com>
2022-04-07 16:25:16 +0200

    ex4 done

--- components/Products.js
+++ components/Products.js
@@ -4,9 +4,9 @@ import Product from './Product';
 export default function Products() {
 	return (
 	    <ScrollView>
-            <Product title='Caca Cola' desc={'Il y a 5 mins'}></Product>
-            <Product title='Pessi' desc={'Hier'}></Product>
-            <Product title='OTocar' desc={'Il y a 2 jours'}></Product>
+            <Product title='Cola' desc={'Il y a 5 mins'}></Product>
+            <Product title='Pepsi' desc={'Hier'}></Product>
+            <Product title='Oasis' desc={'Il y a 2 jours'}></Product>
         </ScrollView>
 	)
 }
