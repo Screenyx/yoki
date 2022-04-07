3a75b433c7495946050faef1224e785ce29bfe31
Screenyx <mercier.hugo14@gmail.com>
2022-04-07 09:40:08 +0200

    ex1 done

--- App.js
+++ App.js
@@ -3,82 +3,72 @@ import { StyleSheet, Text, View, Image, Button, ScrollView } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-      <Image source={{
-        uri: 'https://pbs.twimg.com/media/ErE4QraW8AApHZJ.png'
-      }}
-        style={{width:200, height: 200}}
-      />
-      <Button
-        title="Hello Button"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
-      <ScrollView style={styles.scrollView}>
-        <Text style={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Text>
-      </ScrollView>
+    <ScrollView style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Exercice 1</Text>
+      </View>
+      <View style={styles.container}>
+        <View style={styles.container}>
+          <Text style={styles.miniTitle}>Mon titre</Text>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat tortor in turpis sagittis molestie. Phasellus sagittis leo nisl, in maximus purus elementum vel. Vestibulum sodales metus dictum erat fringilla egestas.
+          </Text>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.miniTitle}>Mon titre</Text>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.text}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat tortor in turpis sagittis molestie. Phasellus sagittis leo nisl, in maximus purus elementum vel. Vestibulum sodales metus dictum erat fringilla egestas.
+          </Text>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.miniTitle}>Mon titre</Text>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.text}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat tortor in turpis sagittis molestie. Phasellus sagittis leo nisl, in maximus purus elementum vel. Vestibulum sodales metus dictum erat fringilla egestas.
+          </Text>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 30,
     flex: 1,
     backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
   text: {
-    color: 'black'
+    color: 'black',
+    marginHorizontal: 40,
+    marginTop: 20,
+
   },
-  scrollView: {
-    backgroundColor: 'white',
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: 'black',
+    borderColor: 'black',
+    borderWidth: 1,
+    paddingTop: 10,
+    marginTop: 10,
+
+  },
+  miniTitle: {
     marginHorizontal: 20,
+    marginTop: 30,
+    fontSize: 20,
+    color: 'black',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    paddingBottom: 20,
+    paddingLeft: 20,
   }
 });
